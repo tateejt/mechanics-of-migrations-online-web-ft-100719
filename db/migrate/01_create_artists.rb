71a85dff9bf716e6273f868338311e0ceb4e951f
Emery Tate <tateejt@gmail.com>
2019-11-17 03:01:32 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -6,10 +6,6 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
